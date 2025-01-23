f5dadd7e5764c5906788cff050ae76ebfccc9a43
seongkwan <seong2808@gmail.com>
2025-01-23 20:32:35 +0900

    intial project setup

---
+++ src/test/java/com/seong/common_project/CommonProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seong.common_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommonProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
